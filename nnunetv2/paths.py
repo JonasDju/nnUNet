81bfec904ebea8b88e2f2df6b964f9546ae68169
Jonas Djurevci <71927957+JonasDju@users.noreply.github.com>
2026-01-09 10:55:54 +0100

    fix: restrict the pytorch version to 2.8.0, based on the recommendation of nnUNet's maintainers chore: remove warning if nnUNet's environment variables are not set

--- nnunetv2/paths.py
+++ nnunetv2/paths.py
@@ -18,10 +18,12 @@ import os
 PLEASE READ documentation/setting_up_paths.md FOR INFORMATION TO HOW TO SET THIS UP
 """
 
+
 nnUNet_raw = os.environ.get('nnUNet_raw')
 nnUNet_preprocessed = os.environ.get('nnUNet_preprocessed')
 nnUNet_results = os.environ.get('nnUNet_results')
 
+"""
 if nnUNet_raw is None:
     print("nnUNet_raw is not defined and nnU-Net can only be used on data for which preprocessed files "
           "are already present on your system. nnU-Net cannot be used for experiment planning and preprocessing like "
@@ -37,3 +39,4 @@ if nnUNet_results is None:
     print("nnUNet_results is not defined and nnU-Net cannot be used for training or "
           "inference. If this is not intended behavior, please read documentation/setting_up_paths.md for information "
           "on how to set this up.")
+"""
